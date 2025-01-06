1d4fbf51a501821981b38fabd61466872da18efb
Aakshant Kumar <kumaraakshant2005@gmail.com>
2025-01-06 16:15:41 +0530

    Update server_chat.py

--- server_chat.py
+++ server_chat.py
@@ -1,5 +1,6 @@
 import subprocess
 
-port = input("Enter the port you eant to use: ")
+port = input("Enter the port you want to use: ")
 subprocess.run(["C:\\Program Files (x86)\\Nmap\\ncat.exe", "-lvp", port])
-# This is  is to create an open server to allow other devices t connect to thjis server
+# This is  is to create an open server to allow other devices to connect to this server
+# Now -lvp is a command to execute the server online process ncat is necessary so mention its path its effective to use rather than just ncat because it sometimes does not work
